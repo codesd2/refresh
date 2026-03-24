d81d75182d27bcad1b882c4b79f2d4062850bb37
codesd2 <itssanjaydutta@gmail.com>
2026-03-24 16:36:12 +0530

    Update proplayproject.js

--- proplayproject.js
+++ proplayproject.js
@@ -1,3 +1,13 @@
+const killWatermark = () => {
+    const el = document.getElementById("watermarkOverlay");
+    if (el) el.remove();
+};
+
+killWatermark(); // Kill immediately if it exists on page load
+
+const observer = new MutationObserver(killWatermark);
+observer.observe(document.body, { childList: true, subtree: true });
+
 (() => {
     // force visibility
     Object.defineProperty(document, 'hidden', { get: () => false });
