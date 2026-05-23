b6c72ac1a845489342a788a03260de3f335ff51d
Sanjay Dutta <itssanjaydutta@gmail.com>
2026-05-23 10:39:30 +0530

    Update btcpro.js

--- btcpro.js
+++ btcpro.js
@@ -7,18 +7,4 @@
         });
     }
 
-    // Run continuously
-    killWatermark();
-    setInterval(killWatermark, 300);
-
-    // Watch dynamic DOM changes
-    const observer = new MutationObserver(() => {
-        killWatermark();
-    });
-
-    observer.observe(document.documentElement, {
-        childList: true,
-        subtree: true
-    });
-
-})();
+   
